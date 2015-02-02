0d9d28a6808c3c5c1a244b28b9dd8a4fcee5ae09
Bart Olsthoorn <bartolsthoorn@gmail.com>
2015-02-02 11:55:13 +0000

    Update github link in layout.jade

--- deploy.py
+++ deploy.py
@@ -28,13 +28,18 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(server, username=username, password=password)
 
-run(ssh, 'ps -p $$')
 # Update source code
 run(ssh, 'cd %s/ice-database; git pull' % destiny)
 
 # Update requirements
 print('Updating requirements (this might take a while)..')
 run(ssh, 'cd %s; pip install -r ice-database/requirements.txt' % destiny)
+
+# Symlink shared files/directories
+run(ssh, 'cd %s/ice-database; rm application/config.py' % destiny)
+run(ssh, 'cd %s/ice-database; ln -s /data/icedb/shared/config.py application/config.py' % destiny)
+run(ssh, 'cd %s/ice-database; rm -rf application/data' % destiny)
+run(ssh, 'cd %s/ice-database; ln -s /data/icedb/shared/data application/data' % destiny)
 
 ssh.close()
 print('Finished deploying to %s@%s' % (username, server))
